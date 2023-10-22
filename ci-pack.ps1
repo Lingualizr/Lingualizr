290ccd40b6de3e115b14330aa095fbc8a3486e32
Stefan Nikolei <stefan@nikolei.eu>
2023-10-22 19:04:34 +0200

    build

--- ci-pack.ps1
+++ ci-pack.ps1
@@ -1,4 +1,4 @@
 dotnet clean -c Release
 
 # Building for packing and publishing.
-dotnet pack -c Release -p:PackageOutputPath="$PSScriptRoot/artifacts" -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+dotnet pack -c Release -p:PackageOutputPath="$PSScriptRoot/artifacts"
