8d1154318cbcfd2a2ed5ea0a3f33ab9213cff545
Stefan Nikolei <stefan@nikolei.eu>
2023-10-22 17:33:30 +0200

    Add explicit restore

--- ci-pack.ps1
+++ ci-pack.ps1
@@ -1,7 +1,6 @@
 dotnet --list-sdks
+dotnet restore
 dotnet clean -c Release
 
-$repositoryUrl = "https://github.com/$env:GITHUB_REPOSITORY"
-
 # Building for packing and publishing.
-dotnet pack -c Release -p:PackageOutputPath="$PSScriptRoot/artifacts" -p:RepositoryUrl=$repositoryUrl
+dotnet pack -c Release -p:PackageOutputPath="$PSScriptRoot/artifacts"
