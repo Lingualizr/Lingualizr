73b26702c22c1679c0456d115a7e14bd3a7dc3f4
Stefan Nikolei <stefan@nikolei.eu>
2023-11-05 14:53:32 +0100

    More configurations for verify
    * Upload received files when test failed
    * configs for received files in gitattributes and editorconfig
    * Add Verify.DiffPlex for better error handling

---
+++ src/Lingualizr.Tests/ModuleInitialization.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Lingualizr.Tests;
+
+public class ModuleInitialization
+{
+    [ModuleInitializer]
+    public static void Initialize() =>
+        VerifyDiffPlex.Initialize();
+}
